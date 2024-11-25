83913adf857db814b3e89431fef59e7ecc4e385b
EastFalls <ali.a.aminian@gmail.com>
2024-11-25 01:01:24 -0500

    first commit

---
+++ p1980.cpp
@@ -0,0 +1,20 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision;
+using hpf = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<10000>>;
+
+int main(int argc, char *argv[])
+{
+  hpf a = 2;
+  hpf b = 3;
+
+  hpf c = sqrt(a)+sqrt(b);
+  c = boost::multiprecision::pow(c,1980);
+  c*=10;
+  c = boost::multiprecision::floor(c);
+  cpp_int d = static_cast<cpp_int>(c);
+  std::cout << d%100  << std::endl;
+
+  return 0;
+}
