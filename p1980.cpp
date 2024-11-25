6c86adaae820f58a9586dfa781ede03cb249df20
EastFalls <ali.a.aminian@gmail.com>
2024-11-25 01:04:44 -0500

    problem description

--- p1980.cpp
+++ p1980.cpp
@@ -1,3 +1,7 @@
+/* IMO LongList 1980
+ * find the digits left and right of the decimal point in the decimal form of the number
+ * (sqrt(3)+sqrt(2))**1980
+ * */
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
